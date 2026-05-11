197f9a65aa372889d615fab0d4369984d304d219
Shinji <monmap13@gmail.com>
2026-05-12 01:28:38 +0700

    Cap nhat PORT de deploy

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ Hướng dẫn trả lời:
 
 # --------------------- Run App ---------------------
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
